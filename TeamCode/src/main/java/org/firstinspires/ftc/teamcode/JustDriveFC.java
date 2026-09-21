f507f0aefb36a9d285896ab9817044dac5eea45c
samschoeffler <samschoeffler05@gmail.com>
2026-09-21 12:33:36 -0400

    Drive Practice Field Centric

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JustDriveFC.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.pedropathing.follower.Follower;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+@TeleOp(name = "JustDriveFC")
+public class JustDriveFC extends OpMode {
+    //drive:)
+    private Follower follower;
+
+    @Override
+    public void init() {
+        //init drive:)
+        follower = Constants.createFollower(hardwareMap);
+
+    }
+
+    @Override
+    public void start() {
+        follower.startTeleopDrive();
+    }
+
+    @Override
+    public void loop() {
+        // drive controls:)
+        follower.setTeleOpDrive(
+                -gamepad1.left_stick_y,
+                -gamepad1.left_stick_x,
+                -gamepad1.right_stick_x,
+                false
+        );
+
+        //final updates for follower:)
+        follower.update();
+    }
+}
